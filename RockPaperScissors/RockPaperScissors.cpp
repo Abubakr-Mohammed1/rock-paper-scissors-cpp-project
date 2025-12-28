6e74cf148b272fd771f8d1e41c1804866c223810
Bakr Mohammed <bakr.mohammed@gmail.com>
2025-12-28 02:59:49 +0200

    first pen in first project

--- RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors.cpp
@@ -1,20 +1,127 @@
-// RockPaperScissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+enum enChoice { Stone = 1, Paper = 2, Scissors = 3 };
+enum enWinner { Player = 1, Computer = 2, Draw = 3 };
+
+int RandomNumber(int From, int To)
+{
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+short ReadNumberOfRounds()
+{
+    short Rounds = 0;
+
+    cout << "How Many Rounds? 1 to 10?\n";
+    cin >> Rounds;
+
+    return Rounds;
+}
+
+short EnterPlayerChoice()
+{
+    short Choice;
+    
+    do
+    {
+        cout << "Your Choice : [1]:Stone, [2]:Paper, [3]:Scissors ? ";
+        cin>>Choice;
+
+    } while (!(Choice >= 1 && Choice <= 3));
+
+    return Choice;
+}
+
+enChoice PlayerChoice()
+{
+    switch (EnterPlayerChoice())
+    {
+    case 1:
+        return enChoice::Stone;
+    case 2:
+        return enChoice::Paper;
+    case 3:
+        return enChoice::Scissors;
+    }
+
+}
+
+short GetComputerChoice()
+{
+    return RandomNumber(1, 3);
+}
+
+enChoice ComputerChoice()
+{
+    switch (GetComputerChoice())
+    {
+    case 1:
+        return enChoice::Stone;
+    case 2:
+        return enChoice::Paper;
+    case 3:
+        return enChoice::Scissors;
+    }
+}
+
+enWinner WhoWin(enChoice Player, enChoice Computer)
+{
+    if (Player == Computer)
+        return enWinner::Draw;
+    else if (Player == enChoice::Stone && Computer == enChoice::Paper)
+        return enWinner::Computer;
+    else if (Player == enChoice::Stone && Computer == enChoice::Scissors)
+        return enWinner::Player;
+    else if (Player == enChoice::Paper && Computer == enChoice::Stone)
+        return enWinner::Player;
+    else if (Player == enChoice::Paper && Computer == enChoice::Scissors)
+        return enWinner::Computer;
+    else if (Player == enChoice::Scissors && Computer == enChoice::Stone)
+        return enWinner::Computer;
+    else
+        return enWinner::Player;
+    
+}
+
+void PrintRoundResult(enWinner Winner, enChoice Player, enChoice Computer)
+{
+    cout << "Player Choice : " << Player << endl;
+    cout << "Computer Choice : " << Computer << endl;
+    cout << "Round Winner : " << Winner;
+}
+
+void BeginRound()
+{
+    short Rounds = ReadNumberOfRounds();
+
+    for (int i = 1;i <= Rounds;i++)
+    {
+        cout << "\nRound [" << i << "] begins :\n\n";
+
+        enChoice Player = PlayerChoice();
+        enChoice Computer = ComputerChoice();
+
+        enWinner Winner = WhoWin(Player, Computer);
+
+        cout << "\n_______________Round [" << i << "]_______________\n\n";
+        PrintRoundResult(Winner, Player, Computer);
+        cout << "\n_______________________________________\n";
+    }
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+
+    BeginRound();
+
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
